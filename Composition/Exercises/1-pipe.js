49a08fc0e5505dc5088e54730cdf33aeef86bcd5
sunfl0wer <therealsunfl0wer@proton.me>
2025-11-15 13:08:15 +0200

    complete: Composition/1-pipe.js

--- Composition/Exercises/1-pipe.js
+++ Composition/Exercises/1-pipe.js
@@ -1,8 +1,12 @@
 "use strict";
 
-const pipe =
-  (...fns) =>
-  (x) =>
-    null;
+const pipe = (...fns) => {
+  for (const i of fns) {
+    if (typeof i !== "function") {
+      throw new Error("One of arguments is not a function.");
+    }
+  }
+  return (x) => fns.reduce((v, f) => f(v), x);
+};
 
 module.exports = { pipe };

82c1794043b8881ef400d39d64b51bdcc5e6b257
sunfl0wer <therealsunfl0wer@proton.me>
2025-11-27 22:35:51 +0200

    complete: HigherOrderFunction

--- HigherOrderFunction/Exercises/3-wrapper.js
+++ HigherOrderFunction/Exercises/3-wrapper.js
@@ -1,5 +1,41 @@
 "use strict";
 
-const contract = (fn, ...types) => null;
+const contract =
+  (fn, ...types) =>
+  (...args) => {
+    const argTypes = types.slice(0, types.length - 1);
+    const resultType = types[types.length - 1];
+
+    if (args.length !== argTypes.length) {
+      throw new TypeError(`Arg count error: Expected ${argTypes.length}`);
+    }
+
+    const check = (val, def, isArg) => {
+      const isPrim = (t, v) =>
+        (t === Number && typeof v === "number") ||
+        (t === String && typeof v === "string") ||
+        (t === Boolean && typeof v === "boolean");
+
+      if (!isPrim(def, val) && !(val instanceof def)) {
+        throw new TypeError(
+          `${isArg ? "Argument" : "Result"} type error: Expected ${def.name}`
+        );
+      }
+    };
+
+    for (let i = 0; i < args.length; i++) {
+      check(args[i], argTypes[i], true);
+    }
+
+    const res = fn(...args);
+    check(res, resultType, false);
+    return res;
+  };
+
+/* const concat = (s1, s2) => s1 + s2;
+const concatStrings = contract(concat, String, String, String);
+const res = concatStrings("Hello ", "world!");
+console.dir(res); // Output: Hello world! 
+*/
 
 module.exports = { contract };

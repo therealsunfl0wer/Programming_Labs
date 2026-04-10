5566754f7a961e94024c3b605a76dfb6ae3ebfc8
sunfl0wer <therealsunfl0wer@proton.me>
2026-04-10 20:30:02 +0300

    start: Task4

---
+++ 4-priority-queue/1-priority-queue.js
@@ -0,0 +1,17 @@
+/*
+Task 4: Implementing a Bi-Directional Priority Queue
+
+Implement a priority queue that allows:
+	•	Retrieving both the highest and lowest priority elements from any end.
+	•	Retrieving elements based on order of insertion (FIFO/LIFO) from either end.
+
+Requirements:
+1.	Core Priority Queue Functionality
+	•	Support inserting elements with an associated priority value.
+	•	Allow peeking and dequeuing elements based on priority order (highest or lowest).
+	•	Allow peeking and dequeuing elements based on insertion order (oldest or newest).
+2.	Operations:
+	•	enqueue(item, priority): Insert an element with a priority.
+	•	dequeue(highest, lowest, oldest, newest)
+	*	peek(highest, lowest, oldest, newest)
+*/

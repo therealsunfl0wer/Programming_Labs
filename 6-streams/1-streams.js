2a4e4eb9dce875f18553e753a270b7ed8d9f6a20
sunfl0wer <therealsunfl0wer@proton.me>
2026-05-16 15:43:12 +0300

    update: Task6 usage

--- 6-streams/1-streams.js
+++ 6-streams/1-streams.js
@@ -8,3 +8,20 @@ const sliceLog = new stream.Transform({
     callback();
   },
 });
+
+// usage (assume an nginx log)
+let logData = `
+  192.168.1.1 - - [21/May/2026:19:15:00] "GET /index.html HTTP/1.1" 200 4523
+  192.168.1.2 - - [21/May/2026:19:15:01] "POST /login HTTP/1.1" 401 231
+  192.168.1.3 - - [21/May/2026:19:15:02] "GET /hidden-api HTTP/1.1" 404 152"
+  192.168.1.4 - - [21/May/2026:19:15:03] "GET /server-error HTTP/1.1" 500 0"
+  `;
+
+let logStream = new stream.Readable({
+  read() {
+    this.push(logData);
+    this.push(null); // EOF
+  },
+});
+
+logStream.pipe(sliceLog).pipe(process.stdout);

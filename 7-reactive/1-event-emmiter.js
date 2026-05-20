1fed645126ad250ffeddb436edd7ddb1969d2499
sunfl0wer <therealsunfl0wer@proton.me>
2026-05-20 18:47:08 +0300

    update: Task7 usage

--- 7-reactive/1-event-emmiter.js
+++ 7-reactive/1-event-emmiter.js
@@ -5,3 +5,21 @@ class MessageBus extends events.EventEmitter {
     this.emit("message", message);
   }
 }
+
+// usage
+
+const bus = new MessageBus();
+
+const messageHandler = (message) => {
+  console.log(`Received message: ${message}`);
+};
+const timeHandler = () => {
+  console.log(`Current time: ${new Date().toLocaleTimeString()}`);
+};
+// sub
+bus.on("message", messageHandler);
+bus.on("message", timeHandler);
+
+// pub
+setTimeout(() => bus.sendMessage("mezcal"), 300);
+bus.sendMessage("beer");
